790e49f4077a20ebe81df39cbff074d0556bee71
Veronika Rykoic <rykoicveronika@gmail.com>
2026-04-24 20:43:10 +0200

    Add files via upload

---
+++ analiza-gestosci-energetycznej-mleka/analiza-gestosci-energetycznej-mleka/scripts/analiza.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+import os
+
+folder = "/Users/mariusz/Documents/Matematyka/sem 6/"
+nazwa_pliku = "dane4-2.xlsx"  
+pelna_sciezka = os.path.join(folder, nazwa_pliku)
+
+try:
+    print(f"Wczytuję arkusz Excel z: {pelna_sciezka}")
+    
+    df = pd.read_excel(pelna_sciezka, skiprows=1)
+    
+    # Czyszczenie nazw kolumn
+    df.columns = df.columns.str.strip()
+    
+    print("Plik wczyta ny, znalezione kolumny:", df.columns.tolist())
+
+    # Konwersja i czyszczenie danych
+    df['energia'] = pd.to_numeric(df['energia'], errors='coerce')
+    df['grupa'] = pd.to_numeric(df['grupa'], errors='coerce')
+    df = df.dropna(subset=['energia', 'grupa'])
+
+    # Podział na grupy (1=Term, 2=Preterm)
+    term = df[df['grupa'] == 1]['energia']
+    preterm = df[df['grupa'] == 2]['energia']
+
+    if len(term) > 0 and len(preterm) > 0:
+        
+        # Statystyka
+        t_stat, p_val = stats.ttest_ind(term, preterm)
+
+        print("\n--- WYNIKI ---")
+        print(f"Średnia Term: {term.mean():.2f}")
+        print(f"Średnia Preterm: {preterm.mean():.2f}")
+        print(f"p-value: {p_val:.4f}")
+
+        # Wykres
+        plt.figure(figsize=(8, 6))
+        df['Typ'] = df['grupa'].map({1: 'Term', 2: 'Preterm'})
+        sns.boxplot(x='Typ', y='energia', data=df, palette='Set2')
+        plt.title('Porównanie energii (Kcal) - dane z pliku Excel')
+        plt.show()
+    else:
+        print("błąd.")
+
+except Exception as e:
+    print(f"błąd")
+
